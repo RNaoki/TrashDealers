d68efb7224b11bf8119cda3ff736ab9d9405a5a8
joaoschwab <joaofelipeschwab@gmail.com>
2020-05-25 21:16:11 -0300

    Mudanca no script e adicionado logos

--- trash/views/assets/js/scripts.js
+++ trash/views/assets/js/scripts.js
@@ -211,6 +211,48 @@ var trash = {
         var e = document.getElementById("category");
         var value = e.options[e.selectedIndex].value;
         trash.adiciona = document.getElementById('quantia').value * value
+    },
+
+    show: function(appear, disapear){
+        appear.style.opacity = "1";
+        disapear.style.opacity = "0";
+        appear.style.height = "200px";
+
+    },
+
+    hide:function (appear, disapear) {
+        appear.style.opacity = "0";
+        disapear.style.opacity = "1";
+        appear.style.height = "0px";
+
+
     }
 
 }
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+    
+
+
